ca312a87f51c20534446e97a3261507ddee23cdf
Gabriel Falcade <80369496+GabrielFalcade97@users.noreply.github.com>
2021-10-11 12:06:43 -0300

    Refatorando Calculadora de Salário

--- Atividade 3/lib/CalculadoraDeSalario.js
+++ Atividade 3/lib/CalculadoraDeSalario.js
@@ -1,6 +1,30 @@
 class CalculadoraDeSalario {
     constructor() {}
   
+    calculaSalarioLiquido(salarioBase, percentualDesconto) {
+      return (salarioBase * (100 - percentualDesconto)) / 100;
+    }
+  
+    salDesenvolvedor(salarioBase) {
+      const percentualDesconto = salarioBase >= 3000.0 ? 20 : 10;
+      return this.calculaSalarioLiquido(salarioBase, percentualDesconto);
+    }
+  
+    salDBA(salarioBase) {
+      const percentualDesconto = salarioBase >= 2000.0 ? 25 : 15;
+      return (salarioBase * (100 - percentualDesconto)) / 100;
+    }
+  
+    salTestador(salarioBase) {
+      const percentualDesconto = salarioBase >= 2000.0 ? 25 : 15;
+      return this.calculaSalarioLiquido(salarioBase, percentualDesconto);
+    }
+  
+    salGerente(salarioBase) {
+      const percentualDesconto = salarioBase >= 5000.0 ? 30 : 20;
+      return this.calculaSalarioLiquido(salarioBase, percentualDesconto);
+    }
+  
     calculaSalario(funcionario) {
       if (
         funcionario.nome.length == 0 ||
@@ -8,54 +32,21 @@ class CalculadoraDeSalario {
         funcionario.salarioBase <= 0 ||
         funcionario.cargo.length == 0
       ) {
-        return "campos 'nome', 'email', 'salarioBase' e 'cargo' precisam ser validos";
+        return "campos 'nome', 'email', 'salarioBase' e 'cargo' tem que ser validos!";
       }
-
+  
       let percentualDesconto = 0;
-
-    switch (funcionario.cargo) {
-      case "desenvolvedor":
-        if (funcionario.salarioBase >= 3000.0) {
-          percentualDesconto = 20;
-        }
-
-        if (funcionario.salarioBase < 3000.0) {
-          percentualDesconto = 10;
-        }
-        return (funcionario.salarioBase * (100 - percentualDesconto)) / 100;
-
+  
+      switch (funcionario.cargo) {
+        case "desenvolvedor":
+          return this.salDesenvolvedor(funcionario.salarioBase);
         case "dba":
-        if (funcionario.salarioBase >= 2000.0) {
-          percentualDesconto = 25;
-        }
-
-        if (funcionario.salarioBase < 2000.0) {
-            percentualDesconto = 15;
-        }
-        return (funcionario.salarioBase * (100 - percentualDesconto)) / 100;
-        
+          return this.salDBA(funcionario.salarioBase);
         case "testador":
-        if (funcionario.salarioBase >= 2000.0) {
-          percentualDesconto = 25;
-        }
-
-        if (funcionario.salarioBase < 2000.0) {
-            percentualDesconto = 15;
-        }
-        return (funcionario.salarioBase * (100 - percentualDesconto)) / 100;
-
+          return this.salTestador(funcionario.salarioBase);
         case "gerente":
-        if (funcionario.salarioBase >= 5000.0) {
-          percentDeseconto = 30;
-        }
-
-        if (funcionario.salarioBase < 5000.0) {
-            percentualDesconto = 20;
-        }
-        return (funcionario.salarioBase * (100 - percentDeseconto)) / 100;
-
-        
-    }
+          return this.salGerente(funcionario.salarioBase);
+      }
     }
   }
   
