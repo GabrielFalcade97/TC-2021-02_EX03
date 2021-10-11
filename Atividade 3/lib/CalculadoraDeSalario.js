72ff6e4f5e7983fd167b9be9943909c56acb9d0a
Gabriel Falcade <80369496+GabrielFalcade97@users.noreply.github.com>
2021-10-11 11:16:41 -0300

    Testando salário desenvolvedor (passando)

--- Atividade 3/lib/CalculadoraDeSalario.js
+++ Atividade 3/lib/CalculadoraDeSalario.js
@@ -8,8 +8,22 @@ class CalculadoraDeSalario {
         funcionario.salarioBase <= 0 ||
         funcionario.cargo.length == 0
       ) {
-        return "campos 'nome', 'email', 'salarioBase' e 'cargo' tem que ser validos!";
+        return "campos 'nome', 'email', 'salarioBase' e 'cargo' precisam ser validos";
       }
+
+      let percentualDesconto = 0;
+
+    switch (funcionario.cargo) {
+      case "desenvolvedor":
+        if (funcionario.salarioBase >= 3000.0) {
+          percentualDesconto = 20;
+        }
+        if (funcionario.salarioBase < 3000.0) {
+          percentualDesconto = 10;
+        }
+        return (funcionario.salarioBase * (100 - percentualDesconto)) / 100;
+    }
+
     }
   }
   
